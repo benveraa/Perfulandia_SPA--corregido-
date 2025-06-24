992d0ab7d24db22121be1dd80d5f8a92c7c9dbbd
benveraa <148820803+benveraa@users.noreply.github.com>
2025-06-24 11:24:22 -0400

    Update Perfulandia_SPA_repository.java

--- Perfulandia_SPA/Perfulandia_SPA/src/main/java/com/Perfulandia/repository/Perfulandia_SPA_repository.java
+++ Perfulandia_SPA/Perfulandia_SPA/src/main/java/com/Perfulandia/repository/Perfulandia_SPA_repository.java
@@ -2,4 +2,9 @@ package com.Perfulandia.repository;
 
 public interface Perfulandia_SPA_repository {
 
+  
+    git config --global user.name "benveraa"
+
+    git config --global user.email "ben.veraa@duocuc.cl"
+  
 }
